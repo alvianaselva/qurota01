d9b7a30dc4b20b0c00ebabf56a6331cc33b8546c
goparklubanic <nugroho.redbuff@gmail.com>
2019-08-12 18:51:48 +0700

    jurnal, agenda berita

--- www/js/index.js
+++ www/js/index.js
@@ -6,4 +6,12 @@ $(document).ready( function(){
     $("#mm_sentra").click( function(){
         window.location="nisen.html";
     })
+
+    $("#mm_agenta").click( function(){
+        window.location="agerita.html";
+    })
+
+    $("#mm_jurnal").click( function(){
+        window.location="jurnalr.html";
+    })
 });
