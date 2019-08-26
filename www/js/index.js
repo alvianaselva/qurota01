2eadab53f9559fc103098ff96ed1ab521bd5353f
alvianaselva <alvianaselva@gmail.com>
2019-08-26 19:43:18 +0700

    fix index.js

--- www/js/index.js
+++ www/js/index.js
@@ -6,9 +6,6 @@ $(document).ready( function(){
     $("#mm_sentra").click( function(){
         window.location="nisen.html";
     })
-<<<<<<< HEAD
-    
-=======
 
     $("#mm_agenta").click( function(){
         window.location="agerita.html";
@@ -21,5 +18,4 @@ $(document).ready( function(){
     $("#mm_kuriku").click( function(){
         window.location="kurikulum.html";
     })
->>>>>>> bd6c1c6a84bfcad38d02e709ad8fcaf5172e346f
 });
